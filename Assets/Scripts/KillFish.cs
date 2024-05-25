b03b2485c2478d0c4787236697ca59b17d360b01
GadS <gads06121994@yandex.ru>
2024-05-25 15:22:59 +0400

    Добавил хуку убийство рыбы

--- Assets/Scripts/KillFish.cs
+++ Assets/Scripts/KillFish.cs
@@ -6,14 +6,33 @@ public class KillFish : MonoBehaviour
 {
     public LayerMask layerMask;
 
+    Score score;
+
+    private void Start()
+    {
+        score = FindObjectOfType<Score>();
+    }
+
     private void OnTriggerEnter(Collider other)
     {
         int collidedLayer = other.gameObject.layer;
 
+        Process(other.gameObject, collidedLayer);
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        int collidedLayer = collision.gameObject.layer;
+
+        Process(collision.gameObject, collidedLayer);
+    }
+
+    void Process(GameObject other, int collidedLayer)
+    {
         if (((1 << collidedLayer) & layerMask) != 0)
         {
-            Destroy(other.attachedRigidbody.gameObject);
-            FindObjectOfType<Score>().AddScore(1);
+            Destroy(other);
+            score.AddScore(1);
         }
     }
 }
